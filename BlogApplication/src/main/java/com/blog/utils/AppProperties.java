bb6b0da9da0238ebaaef08f8ef2d8a74bef0eba6
= <=>
2026-04-03 11:29:13 +0530

    Initial commit

---
+++ BlogApplication/src/main/java/com/blog/utils/AppProperties.java
@@ -0,0 +1,9 @@
+package com.blog.utils;
+
+
+public class AppProperties 
+{
+   public static final String CLOUDINARY_CLOUD_NAME ="druhdw3tu";
+   public static final String CLOUDINARY_API_KEY="341665919363159";
+   public static final String CLOUDINARY_SECRET_KEY="MJ6fsaK10WXT6iHWfUG2HZtqLl8";
+}
